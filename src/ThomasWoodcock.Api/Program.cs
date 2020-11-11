77e39a76759e54f35c2dc51c7183f51ef9da01a4
Thomas Woodcock <thomas@thomaswoodcock.net>
2020-11-11 07:23:39 +0000

    Makes Program top-level

--- src/ThomasWoodcock.Api/Program.cs
+++ src/ThomasWoodcock.Api/Program.cs
@@ -1,25 +1,16 @@
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace ThomasWoodcock.Api
-{
-    internal class Program
-    {
-        private async static Task Main()
-        {
-            await CreateHostBuilder()
+using ThomasWoodcock.Api;
+
+await CreateHostBuilder()
                 .Build()
                 .RunAsync()
                 .ConfigureAwait(true);
-        }
 
-        private static IHostBuilder CreateHostBuilder() =>
-            Host.CreateDefaultBuilder()
-                .ConfigureWebHostDefaults(builder =>
-                {
-                    builder.UseStartup<Startup>();
-                });
-    }
-}
+static IHostBuilder CreateHostBuilder() =>
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(builder =>
+        {
+            builder.UseStartup<Startup>();
+        });
