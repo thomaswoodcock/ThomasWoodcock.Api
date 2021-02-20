8b5f0ae6cdf5ee16e0fc69c09093d3290c615593
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-02-20 12:27:04 +0000

    Adds missing unit tests (#17)

--- src/ThomasWoodcock.Service.Domain/Accounts/FailureReasons/InvalidAccountIdFailure.cs
+++ src/ThomasWoodcock.Service.Domain/Accounts/FailureReasons/InvalidAccountIdFailure.cs
@@ -7,7 +7,7 @@ namespace ThomasWoodcock.Service.Domain.Accounts.FailureReasons
     ///     An extension of the <see cref="InvalidFormatFailure" /> class that represents a failure that occurs when attempting
     ///     to create an account with an invalid ID.
     /// </summary>
-    internal sealed class InvalidAccountIdFailure : InvalidFormatFailure
+    public sealed class InvalidAccountIdFailure : InvalidFormatFailure
     {
     }
 }
