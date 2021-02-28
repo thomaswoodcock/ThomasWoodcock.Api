27321e1bec4d5d16c8a1b6b8ea39b15c0cd262a0
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-02-28 09:09:22 +0000

    Adds XML docs

--- src/ThomasWoodcock.Service.Application/Common/DomainEvents/ServiceCollectionExtensions.cs
+++ src/ThomasWoodcock.Service.Application/Common/DomainEvents/ServiceCollectionExtensions.cs
@@ -8,8 +8,17 @@ using ThomasWoodcock.Service.Application.Common.Notifications;
 
 namespace ThomasWoodcock.Service.Application.Common.DomainEvents
 {
+    /// <summary>
+    ///     Domain event-related extension methods for <see cref="IServiceCollection" /> objects.
+    /// </summary>
     internal static class ServiceCollectionExtensions
     {
+        /// <summary>
+        ///     Registers domain event-related services with the given <paramref name="collection" />.
+        /// </summary>
+        /// <param name="collection">
+        ///     The <see cref="IServiceCollection" /> with which to register the services.
+        /// </param>
         public static void AddDomainEvents(this IServiceCollection collection)
         {
             // TODO: Remove when notifications implemented.
