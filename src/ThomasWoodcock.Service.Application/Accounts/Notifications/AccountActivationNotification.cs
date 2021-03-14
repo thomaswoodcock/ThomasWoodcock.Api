0f5fe4128d9279c426580d093acf15239ba9832e
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-03-14 16:10:37 +0000

    Convert AccountActivationNotification to record

--- src/ThomasWoodcock.Service.Application/Accounts/Notifications/AccountActivationNotification.cs
+++ src/ThomasWoodcock.Service.Application/Accounts/Notifications/AccountActivationNotification.cs
@@ -1,44 +1,20 @@
-using System;
-
 using ThomasWoodcock.Service.Application.Accounts.Entities;
 using ThomasWoodcock.Service.Application.Common.Notifications;
 using ThomasWoodcock.Service.Domain.Accounts;
 
 namespace ThomasWoodcock.Service.Application.Accounts.Notifications
 {
-    /// <inheritdoc />
+    /// <inheritdoc cref="INotification" />
     /// <summary>
     ///     An implementation of the <see cref="INotification" /> interface used for notifying users that their account needs
     ///     to be activated.
     /// </summary>
-    internal sealed class AccountActivationNotification : INotification
-    {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="AccountActivationNotification" /> class.
-        /// </summary>
-        /// <param name="account">
-        ///     The account that required activation.
-        /// </param>
-        /// <param name="activationKey">
-        ///     The key that can be used to activate the account.
-        /// </param>
-        /// <exception cref="ArgumentNullException">
-        ///     Thrown if the given <paramref name="account" /> or <paramref name="activationKey" /> are null.
-        /// </exception>
-        public AccountActivationNotification(Account account, AccountActivationKey activationKey)
-        {
-            this.Account = account ?? throw new ArgumentNullException(nameof(account));
-            this.ActivationKey = activationKey ?? throw new ArgumentNullException(nameof(activationKey));
-        }
-
-        /// <summary>
-        ///     Gets the account that required activation.
-        /// </summary>
-        public Account Account { get; }
-
-        /// <summary>
-        ///     Gets the key that can be used to activate the account.
-        /// </summary>
-        public AccountActivationKey ActivationKey { get; }
-    }
+    /// <param name="Account">
+    ///     The account that requires activation.
+    /// </param>
+    /// <param name="ActivationKey">
+    ///     The key that can be used to activate the account.
+    /// </param>
+    internal sealed record AccountActivationNotification
+        (Account Account, AccountActivationKey ActivationKey) : INotification;
 }
