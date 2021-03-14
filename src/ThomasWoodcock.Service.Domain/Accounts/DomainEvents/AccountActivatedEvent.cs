82bccfedf921f1743b29704c6ae27fcaf938dc6b
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-03-14 15:47:51 +0000

    Convert domain events to records

--- src/ThomasWoodcock.Service.Domain/Accounts/DomainEvents/AccountActivatedEvent.cs
+++ src/ThomasWoodcock.Service.Domain/Accounts/DomainEvents/AccountActivatedEvent.cs
@@ -1,30 +1,14 @@
-using System;
-
 using ThomasWoodcock.Service.Domain.SeedWork;
 
 namespace ThomasWoodcock.Service.Domain.Accounts.DomainEvents
 {
-    /// <inheritdoc />
+    /// <inheritdoc cref="IDomainEvent" />
     /// <summary>
     ///     An implementation of the <see cref="IDomainEvent" /> interface that represents a domain event that is raised when
     ///     an account has been activated.
     /// </summary>
-    public sealed class AccountActivatedEvent : IDomainEvent
-    {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="AccountActivatedEvent" /> class.
-        /// </summary>
-        /// <param name="account">
-        ///     The account that was activated.
-        /// </param>
-        public AccountActivatedEvent(Account account)
-        {
-            this.Account = account ?? throw new ArgumentNullException(nameof(account));
-        }
-
-        /// <summary>
-        ///     Gets the account that was activated.
-        /// </summary>
-        public Account Account { get; }
-    }
+    /// <param name="Account">
+    ///     The account that was activated.
+    /// </param>
+    public sealed record AccountActivatedEvent(Account Account) : IDomainEvent;
 }
--- src/ThomasWoodcock.Service.Domain/Accounts/DomainEvents/AccountCreatedEvent.cs
+++ src/ThomasWoodcock.Service.Domain/Accounts/DomainEvents/AccountCreatedEvent.cs
@@ -1,30 +1,14 @@
-using System;
-
 using ThomasWoodcock.Service.Domain.SeedWork;
 
 namespace ThomasWoodcock.Service.Domain.Accounts.DomainEvents
 {
-    /// <inheritdoc />
+    /// <inheritdoc cref="IDomainEvent" />
     /// <summary>
     ///     An implementation of the <see cref="IDomainEvent" /> interface that represents a domain event that is raised when
     ///     an account has been created.
     /// </summary>
-    public sealed class AccountCreatedEvent : IDomainEvent
-    {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="AccountCreatedEvent" /> class.
-        /// </summary>
-        /// <param name="account">
-        ///     The account that was created.
-        /// </param>
-        public AccountCreatedEvent(Account account)
-        {
-            this.Account = account ?? throw new ArgumentNullException(nameof(account));
-        }
-
-        /// <summary>
-        ///     Gets the account that was created.
-        /// </summary>
-        public Account Account { get; }
-    }
+    /// <param name="Account">
+    ///     The account that was created.
+    /// </param>
+    public sealed record AccountCreatedEvent(Account Account) : IDomainEvent;
 }
