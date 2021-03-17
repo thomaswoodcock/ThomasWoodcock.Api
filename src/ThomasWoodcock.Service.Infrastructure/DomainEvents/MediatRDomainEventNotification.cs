2105697d5aa0504c5baf92ff38107795a106e052
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-03-17 14:21:48 +0000

    Adds MediatRDomainEventNotification

---
+++ src/ThomasWoodcock.Service.Infrastructure/DomainEvents/MediatRDomainEventNotification.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+using ThomasWoodcock.Service.Domain.SeedWork;
+
+namespace ThomasWoodcock.Service.Infrastructure.DomainEvents
+{
+    /// <inheritdoc cref="INotification" />
+    /// <summary>
+    ///     An implementation of the <see cref="INotification" /> interface used to publish domain events using MediatR.
+    /// </summary>
+    /// <typeparam name="T">
+    ///     The type of domain event that will be published.
+    /// </typeparam>
+    /// <param name="DomainEvent">
+    ///     The domain event that will be published.
+    /// </param>
+    internal sealed record MediatRDomainEventNotification<T>(T DomainEvent) : INotification
+        where T : class, IDomainEvent;
+}
