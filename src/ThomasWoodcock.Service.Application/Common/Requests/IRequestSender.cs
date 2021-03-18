45748687ddcf303fd85c96de4a7c01b49f4e2715
Thomas Woodcock <thomas@thomaswoodcock.net>
2021-03-18 11:37:29 +0000

    Adds request interfaces

---
+++ src/ThomasWoodcock.Service.Application/Common/Requests/IRequestSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThomasWoodcock.Service.Application.Common.Requests
+{
+    /// <summary>
+    ///     Allows a class to act as a sender of application requests.
+    /// </summary>
+    public interface IRequestSender
+    {
+        /// <summary>
+        ///     Sends the given application request to its respective handler.
+        /// </summary>
+        /// <param name="request">
+        ///     The application request to send.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     The cancellation token used to terminate the operation, if requested.
+        /// </param>
+        /// <typeparam name="TResponse">
+        ///     The type of response that will be returned.
+        /// </typeparam>
+        /// <returns>
+        ///     A response of type <typeparamref name="TResponse" />.
+        /// </returns>
+        Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken);
+    }
+}
